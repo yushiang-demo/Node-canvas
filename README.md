49a0684dd873146c79908b255982440df1365b07
yushiang <tsengyushiang@gmail.com>
2023-09-25 12:17:25 +0800

    feature: node canvas image grid generator.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Node-canvas
 export image grid
+
+
+## Quick start
+
+### Setup
+
+```
+npm install
+```
+
+### Commands
+
+```
+PS C:\Users\node-canvas-image-grid> node index.js -h
+Usage: index [options]
+
+Options:
+  -i, --inputs [strings...]     input filenames
+  -o, --output <string>         output filename
+  -r, --resolution [number...]  output resolution [width,height]
+  -p, --padding [number...]     padding [x,y]
+  -h, --help                    display help for command
+```
+
+### Example
+
+```
+node index.js -i https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png -o ./output.png -r 100 100 -p 10 1
+```
+
+![example](./output.png)
---
+++ index.js
@@ -0,0 +1,65 @@
+const { program } = require("commander");
+const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs");
+
+const main = async (inputs, output, width, height, paddingX, paddingY) => {
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
+
+  const images = await Promise.all(
+    inputs.map((filename) => loadImage(filename))
+  );
+
+  const row = Math.ceil(Math.sqrt(inputs.length));
+  const col = Math.ceil(inputs.length / row);
+
+  const xBlockOffset = width / row;
+  const yBlockOffset = height / col;
+
+  const blockWidth = xBlockOffset - paddingX;
+  const blockHeight = yBlockOffset - paddingY;
+
+  images.forEach((image, index) => {
+    const imageRow = Math.trunc(index / col);
+    const imageCol = index % col;
+
+    const { width, height } = image;
+    const applySale = Math.min(blockWidth / width, blockHeight / height);
+
+    const renderWidth = width * applySale;
+    const renderHeight = height * applySale;
+
+    const renderOriginX =
+      xBlockOffset * imageRow + xBlockOffset / 2 - renderWidth / 2;
+    const renderOriginY =
+      yBlockOffset * imageCol + yBlockOffset / 2 - renderHeight / 2;
+
+    ctx.drawImage(
+      image,
+      renderOriginX,
+      renderOriginY,
+      renderWidth,
+      renderHeight
+    );
+  });
+
+  fs.writeFileSync(output, canvas.toBuffer());
+};
+
+program
+  .option("-i, --inputs [strings...]", "input filenames")
+  .option("-o, --output <string>", "output filename")
+  .option("-r, --resolution [number...]", "output resolution [width,height]")
+  .option("-p, --padding [number...]", "padding [x,y]");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.inputs && options.output && options.resolution) {
+  const [width, height] = options.resolution.map((value) => parseInt(value));
+  const [paddingX, paddingY] = options.padding.map((value) => parseInt(value));
+  main(options.inputs, options.output, width, height, paddingX, paddingY);
+} else {
+  console.log(`arguments not found, current args: ${JSON.stringify(options)}`);
+}
